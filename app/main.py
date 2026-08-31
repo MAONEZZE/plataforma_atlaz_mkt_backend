c3a438702ce77272859b90892b5fe53a16996cae
MAONEZZE <ruansanchezskt@gmail.com>
2026-08-31 18:25:24 -0300

    feat: update FastAPI configuration to disable docs and openapi URLs in production environment

--- app/main.py
+++ app/main.py
@@ -85,7 +85,8 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
 app = FastAPI(
     title="akeel Backend",
     version="0.1.0",
-    docs_url="/docs" if settings.ENVIRONMENT != "prod" else "dev",
+    docs_url=None if settings.ENVIRONMENT == "prod" else "/docs",
+    openapi_url=None if settings.ENVIRONMENT == "prod" else "/openapi.json",
     redoc_url=None,
     lifespan=lifespan,
 )
